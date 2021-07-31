dcb36dcb21cbdd066167b8dc04177592c8adf12b
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2021-07-31 21:26:11 +0500

    feat: add projects component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,15 @@
+import Projects from "../projects/Projects";
+import { Container } from "react-bootstrap";
+import data from '../../data';
+
 function App() {
-    return (
-        <h1>Salom</h1>
-    );
+  return (
+    <Container>
+      <div>
+        <Projects projectsInfo={data} />
+      </div>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/data.tsx
@@ -0,0 +1,60 @@
+import projectImg from "./projectImages/project-img.png";
+
+const data = [
+  {
+    name: "calculator",
+    image: projectImg,
+    deployPath: "https://calculator-00.netlify.app/",
+    sourcePath: "https://github.com/Husan-Eshonqulov/my-projects",
+  },
+  {
+    name: "calculator",
+    image: projectImg,
+    deployPath: "https://calculator-00.netlify.app/",
+    sourcePath: "https://github.com/Husan-Eshonqulov/my-projects",
+  },
+  {
+    name: "calculator",
+    image: projectImg,
+    deployPath: "https://calculator-00.netlify.app/",
+    sourcePath: "https://github.com/Husan-Eshonqulov/my-projects",
+  },
+  {
+    name: "calculator",
+    image: projectImg,
+    deployPath: "https://calculator-00.netlify.app/",
+    sourcePath: "https://github.com/Husan-Eshonqulov/my-projects",
+  },
+  {
+    name: "calculator",
+    image: projectImg,
+    deployPath: "https://calculator-00.netlify.app/",
+    sourcePath: "https://github.com/Husan-Eshonqulov/my-projects",
+  },
+  {
+    name: "calculator",
+    image: projectImg,
+    deployPath: "https://calculator-00.netlify.app/",
+    sourcePath: "https://github.com/Husan-Eshonqulov/my-projects",
+  },
+  {
+    name: "calculator",
+    image: projectImg,
+    deployPath: "https://calculator-00.netlify.app/",
+    sourcePath: "https://github.com/Husan-Eshonqulov/my-projects",
+  },
+  {
+    name: "calculator",
+    image: projectImg,
+    deployPath: "https://calculator-00.netlify.app/",
+    sourcePath: "https://github.com/Husan-Eshonqulov/my-projects",
+  },
+  {
+    name: "calculator",
+    image: projectImg,
+    deployPath: "https://calculator-00.netlify.app/",
+    sourcePath: "https://github.com/Husan-Eshonqulov/my-projects",
+  },
+];
+
+export default data;
